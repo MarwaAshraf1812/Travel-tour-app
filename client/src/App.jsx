e48550a50f9f9bc4f863faa64b02dc2cf1afb092
MarwaAshraf1812 <marashraf090@gmail.com>
2024-10-31 21:05:25 +0300

    feat: create navbar section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,6 @@
-import { useState } from 'react'
-import { Button } from './components/ui/button'
-import { Calendar } from './components/ui/calendar'
-
+import Home from "../src/pages/Home";
 export default function App() {
-  const [date, setDate] = useState(new Date())
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-red-500 text-center">Hello world!</h1>
-      <Button>Click me!</Button>
-      <Calendar
-        mode="single"
-        selected={date}
-        onSelect={setDate}
-        className="rounded-md border"
-      />
-    </>
+    <Home />
   )
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,7 +9,9 @@ export default {
         md: 'calc(var(--radius) - 2px)',
         sm: 'calc(var(--radius) - 4px)',
       },
-      colors: {},
+      colors: {
+        orange: "#DF6951",
+      },
     },
   },
   plugins: [require('tailwindcss-animate')],
