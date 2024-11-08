fe5222486a85ba941b49f4c01d8f6a38685dc5c3
Amr Khaled <amrk6965@gmail.com>
2024-11-08 10:54:15 -0800

    feat: bannar & Gallery

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,11 +1,25 @@
+import AboutHero from '../components/aboutSections/AboutHero/AboutHero'
+import Promo from '../components/aboutSections/Promo/Promo';
+import Trend from '../components/aboutSections/Trend/Trend';
+import Footer from '../components/layout/Footer'
+import Navbar from '../components/layout/Navbar'
+import Testmonial from '../components/sections/Testmonial';
 import Banner from '../components/aboutSections/Banner/Banner';
 import Gallery from '../components/aboutSections/Gallery/Gallery';
 function About() {
   return (
-    <div>
-      <Banner />
-      <Gallery/>
-    </div>
+    <>
+      <Navbar />
+      <div>
+        <AboutHero />
+        <Promo />
+        <Banner/>
+        <Trend />
+        <Gallery/>
+        <Testmonial />
+      </div>
+      <Footer />
+    </>
   )
 }
 
